bfe7a759a8b8d1c70827461193451746b7dd3e98
GitHub CI <ci@github.com>
2025-06-03 20:55:38 +0000

    Add changes for PR

--- service.go
+++ service.go
@@ -1,9 +1,12 @@
 package main
 
+import "log"
+
 type UserService struct {
 	db Database
 }
 
 func (s *UserService) CreateUser(name string) error {
+	log.Printf("Creating user: %s", name)
 	return s.db.Insert(name)
 }
